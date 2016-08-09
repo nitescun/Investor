d4ef6b34d819e3f4da411791a5ecca9ff20550ec
Nicolae Nitescu <nitescun@yahoo.com>
2016-08-09 07:47:10 -0700

    Initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  # users.password_hash in the database is a :string
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def authenticate(input_password)
+    self.password == input_password
+  end
+
+end
