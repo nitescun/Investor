446fae4b920e183306713219220c1571d3b8950c
Nicolae Nitescu <nitescun@yahoo.com>
2016-08-15 15:22:06 -0700

    Add user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,4 +20,38 @@ class User < ActiveRecord::Base
     self.password == input_password
   end
 
+  def favorite_stocks
+    @stocks = []
+    self.stocks.each do |loop_stock|
+      @stocks << loop_stock.quote
+    end
+  StockQuote::Stock.quote(@stocks)
+  end
+
+  def high_dividend
+    @hdiv_raw = []
+    self.all_stocks.each do |loop_stock|
+      if !loop_stock.dividend_yield 
+       @hdiv_raw << [loop_stock.symbol, 0, loop_stock.name, loop_stock.bid, loop_stock.percent_change]  
+      else
+       @hdiv_raw << [loop_stock.symbol, loop_stock.dividend_yield, loop_stock.name, loop_stock.bid, loop_stock.percent_change]
+     end
+    end 
+    @hdiv_raw.sort_by {|e| e[1]} 
+  end
+
+  def all_stocks
+    @all_stocks_quote = []
+    Stock.all.each do |loop_stock|
+      @all_stocks_quote << loop_stock.quote
+    end
+    @all_stocks = StockQuote::Stock.quote(@all_stocks_quote)
+  end
+
+  def value_stocks
+    
+    
+  end
+
+
 end
